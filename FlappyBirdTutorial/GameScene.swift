588807c7d5603b8f979778e7eca81180bec271f3
Nipuna <nipuna@app-monkeyz.com>
2016-07-28 08:38:36 +0530

    Initial Commit, tutorial part 1, Added ghost, walls, ground

--- FlappyBirdTutorial/GameScene.swift
+++ FlappyBirdTutorial/GameScene.swift
@@ -8,38 +8,101 @@
 
 import SpriteKit
 
+
+struct PhysicsCategory {
+    static let Ghost : UInt32 = 0x1 << 1
+    static let Ground : UInt32 = 0x1 << 2
+    static let Wall : UInt32 = 0x1 << 3
+}
+
 class GameScene: SKScene {
+    
+    var Ground = SKSpriteNode()
+    var Ghost = SKSpriteNode()
+    
+    
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
         
-        self.addChild(myLabel)
+        Ground = SKSpriteNode(imageNamed: "Ground")
+        Ground.setScale(0.5)
+        Ground.position = CGPoint(x: self.frame.width / 2, y: 0 + Ground.frame.height / 2)
+        
+        Ground.physicsBody = SKPhysicsBody(rectangleOfSize: Ground.size)
+        Ground.physicsBody?.categoryBitMask = PhysicsCategory.Ground
+        Ground.physicsBody?.collisionBitMask = PhysicsCategory.Ghost
+        Ground.physicsBody?.contactTestBitMask = PhysicsCategory.Ghost
+        Ground.physicsBody?.affectedByGravity = false
+        Ground.physicsBody?.dynamic = false
+        
+        Ground.zPosition = 3
+        self.addChild(Ground)
+        
+        
+        Ghost = SKSpriteNode(imageNamed: "Ghost")
+        Ghost.size = CGSize(width: 60, height: 70)
+        Ghost.position = CGPoint(x: self.frame.width / 2 - Ghost.frame.width, y: self.frame.height / 2)
+        
+        Ghost.physicsBody = SKPhysicsBody(circleOfRadius: Ghost.frame.height / 2)
+        Ghost.physicsBody?.categoryBitMask = PhysicsCategory.Ghost
+        Ghost.physicsBody?.collisionBitMask = PhysicsCategory.Ground | PhysicsCategory.Wall
+        Ghost.physicsBody?.contactTestBitMask = PhysicsCategory.Ground | PhysicsCategory.Wall
+        Ghost.physicsBody?.affectedByGravity = true
+        Ghost.physicsBody?.dynamic = true
+        
+        Ghost.zPosition = 2
+        self.addChild(Ghost)
+        
+        createWalls()
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+        Ghost.physicsBody?.velocity = CGVectorMake(0, 0)
+        Ghost.physicsBody?.applyImpulse(CGVectorMake(10, 90))
+       
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    
+    func createWalls() {
+        let wallPair = SKNode()
+        
+        let topWall = SKSpriteNode(imageNamed: "Wall")
+        let bottomWall = SKSpriteNode(imageNamed: "Wall")
+        
+        topWall.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2 + 350)
+        bottomWall.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2 - 350)
+        
+        topWall.setScale(0.5)
+        bottomWall.setScale(0.5)
+        
+        topWall.physicsBody = SKPhysicsBody(rectangleOfSize: topWall.size)
+        topWall.physicsBody?.categoryBitMask = PhysicsCategory.Wall
+        topWall.physicsBody?.collisionBitMask = PhysicsCategory.Ghost
+        topWall.physicsBody?.contactTestBitMask = PhysicsCategory.Ghost
+        topWall.physicsBody?.dynamic = false
+        topWall.physicsBody?.affectedByGravity = false
+
+        bottomWall.physicsBody = SKPhysicsBody(rectangleOfSize: bottomWall.size)
+        bottomWall.physicsBody?.categoryBitMask = PhysicsCategory.Wall
+        bottomWall.physicsBody?.collisionBitMask = PhysicsCategory.Ghost
+        bottomWall.physicsBody?.contactTestBitMask = PhysicsCategory.Ghost
+        bottomWall.physicsBody?.dynamic = false
+        bottomWall.physicsBody?.affectedByGravity = false
+
+        
+        topWall.zRotation = CGFloat(M_PI)
+        
+        wallPair.addChild(topWall)
+        wallPair.addChild(bottomWall)
+        
+        wallPair.zPosition = 1
+        
+        self.addChild(wallPair)
     }
 }
